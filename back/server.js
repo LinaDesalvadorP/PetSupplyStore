9e897c4885d6798b6eabf385a682f300fb1f01fe
Lina Desalvador <lindesalvador@gmail.com>
2022-10-09 18:35:25 -0500

    Database connection

--- back/server.js
+++ back/server.js
@@ -1,8 +1,12 @@
 const app=require("./app");
+const connectDatabase = require("./config/database");
 
 //Settear el archivo de configuración
 const dotenv=require("dotenv");
 dotenv.config({path:'back/config/config.env'});
+
+//Configuración de la base de datos
+connectDatabase();
 
 //Llama al servidor
 const server=app.listen(process.env.PORT, ()=>{
