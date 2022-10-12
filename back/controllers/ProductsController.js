a9b73a109de7246967d2270f5cd4d72caa8bd4a8
Lina Desalvador <lindesalvador@gmail.com>
2022-10-11 19:42:49 -0500

    POST de producto.
    Se agregó el controlador y la ruta necesarios para hacer POST de un producto.

--- back/controllers/ProductsController.js
+++ back/controllers/ProductsController.js
@@ -1,6 +1,17 @@
+const product=require("../models/products")
+//Ver lista de productos
 exports.getProducts=(req,res,next)=>{
     res.status(200).json({
         success: true,
         message: "Lista de productos"
+    })
+}
+
+//Crear nuevo producto /api/products
+exports.createProduct=async(req, res, next)=>{
+    const newProduct=await product.create(req.body);
+    res.status(201).json({
+        success:true,
+        newProduct
     })
 }
--- back/routes/products.js
+++ back/routes/products.js
@@ -1,8 +1,11 @@
 const express= require("express");
 const router=express.Router();
 
-const {getProducts} = require("../controllers/ProductsController")
+const {getProducts, createProduct} = require("../controllers/ProductsController")
 
 //establece la ruta en la que veremos getProducts
-router.route('/products').get(getProducts)
+router.route('/products').get(getProducts);
+//Ruta para el post de crear productos
+router.route('/products/new').post(createProduct);
+
 module.exports=router;
