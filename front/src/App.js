6aa0a4e36019b157ea0b29a9124cbe583f49f0ae
Lina Desalvador <lindesalvador@gmail.com>
2022-10-13 17:09:24 -0500

    Se inicializo el front

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+      <img src="./images/coala.png" alt="Logo de coala"></img>
+    </div>
+  );
+}
+
+export default App;
