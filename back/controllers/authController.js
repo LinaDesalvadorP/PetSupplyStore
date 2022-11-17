aed3c728b61c8bc93bf0a0ae965150824b382f72
Lina Desalvador <lindesalvador@gmail.com>
2022-11-17 13:39:59 -0500

    Cerrar sesión#

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -43,3 +43,16 @@ exports.loginUser = catchAsyncErrors(async(req, res, next)=>{
 
     tokenSend(user, 200, res)
 })
+
+//Logout (Cerrar sesión)
+exports.logOut = catchAsyncErrors(async(req,res,next)=>{
+    res.cookie("token", null,{ 
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: "Logged out"
+    })
+})
--- back/routes/auths.js
+++ back/routes/auths.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, loginUser, logOut } = require("../controllers/authController");
+const { isAuthenticatedUser } = require("../middleware/auth");
 const router = express.Router();
 
 router.route('/user/register').post(registerUser)
 router.route('/login').get(loginUser)
-
+router.route('/logout').get(isAuthenticatedUser, logOut)
 module.exports = router
